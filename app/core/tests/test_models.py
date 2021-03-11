ef4bdf3dc6880c969d639b9dd76254ad1ab5b9d8
Jonathan <jonathan.george.dev@gmail.com>
2021-03-10 19:31:31 -0800

    email validation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,3 +21,8 @@ def test_new_user_email_normalized(self):
     user = get_user_model().objects.create_user(email, 'test123')
 
     self.assertEqual(user.email.lower())
+
+def test_new_user_invalid_email(self):
+    """Test creating user with no email raises error"""
+    with self.assertRaises(ValueError):
+        get_user_model().objects.create_user(None, 'test123')
